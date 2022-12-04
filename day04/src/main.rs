5d0042f0182fb293f5b2ca2fb26b91859f6d35f4
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2022-12-04 13:55:59 +0100

    First puzzle

---
+++ day04/src/main.rs
@@ -0,0 +1,42 @@
+use anyhow::{anyhow, Result};
+use itertools::Itertools;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<()> {
+    let f = File::open("input")?;
+
+    let r = BufReader::new(f);
+    let input = r
+        .lines()
+        .map(|l| -> Result<(u64, u64, u64, u64)> {
+            l?.split(&[',', '-'])
+                .flat_map(|v| -> Result<u64> { Ok(v.parse::<u64>()?) })
+                .next_tuple()
+                .ok_or_else(|| anyhow!("Invalid input"))
+        })
+        .collect::<Result<Vec<(u64, u64, u64, u64)>>>()?;
+
+    let output_1 = puzzle_1(&input);
+    println!("puzzle1: {output_1}");
+    assert_eq!(562, output_1);
+
+    // let output_2 = puzzle_2(&input)?;
+    // println!("puzzle2: {output_2}");
+    // assert_eq!(, output_2);
+
+    Ok(())
+}
+
+fn puzzle_1(input: &[(u64, u64, u64, u64)]) -> u64 {
+    let mut sum_overlapping = 0;
+    for (x, y, a, b) in input {
+        if x <= a && y >= b || x >= a && y <= b {
+            sum_overlapping += 1;
+        }
+    }
+
+    sum_overlapping
+}

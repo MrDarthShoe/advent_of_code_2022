e13a75729ec367464e03c17fb27b639a204eb74a
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2022-12-04 14:05:26 +0100

    Day 4 done

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -23,18 +23,29 @@ fn main() -> Result<()> {
     println!("puzzle1: {output_1}");
     assert_eq!(562, output_1);
 
-    // let output_2 = puzzle_2(&input)?;
-    // println!("puzzle2: {output_2}");
-    // assert_eq!(, output_2);
+    let output_2 = puzzle_2(&input);
+    println!("puzzle2: {output_2}");
+    assert_eq!(924, output_2);
 
     Ok(())
 }
 
 fn puzzle_1(input: &[(u64, u64, u64, u64)]) -> u64 {
-    let mut sum_overlapping = 0;
+    let mut sum_containing = 0;
     for (x, y, a, b) in input {
         if x <= a && y >= b || x >= a && y <= b {
-            sum_overlapping += 1;
+            sum_containing += 1;
+        }
+    }
+
+    sum_containing
+}
+
+fn puzzle_2(input: &[(u64, u64, u64, u64)]) -> u64 {
+    let mut sum_overlapping = input.len() as u64;
+    for (x, y, a, b) in input {
+        if (a > x && a > y) || (b < x && b < y) {
+            sum_overlapping -= 1;
         }
     }
 
